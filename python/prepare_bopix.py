5c50a904500a8489c6749e157c11d1fd28ee3a8c
Karim Benabed <benabed@iap.fr>
2011-05-23 23:45:00 +0000

    add tools

---
+++ python/prepare_bopix.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+import sys
+sys.path = ["$REPLACEPATH"]+sys.path
+
+import numpy as nm
+import numpy.random as ra
+import numpy.linalg as la
+import clik.parobject as php
+import clik
+import re
+
+def main(argv):
+  pars = clik.miniparse(argv[1])
+
+  
+import sys
+if __name__=="__main__":
+  main(sys.argv)
