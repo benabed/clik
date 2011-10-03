f7a69e0f8e842e89564e1bad89ee6da6a8b3caef
Karim Benabed <benabed@iap.fr>
2011-10-03 12:36:53 +0000

    1.6

---
+++ src/python/clik_example_py.py
@@ -0,0 +1,17 @@
+#! $PYTHONEXE
+import sys
+sys.path = ["$REPLACEPATH"]+sys.path
+
+import numpy as nm
+import clik
+
+def main(argv):
+  lklfile = argv[1]
+  lkl = clik.clik(lklfile)
+  for clfile in argv[2:]:
+    cls = nm.loadtxt(clfile)
+    nres = lkl(cls.flat[:])
+    print nres
+
+if __name__=="__main__":
+  main(sys.argv)
---
+++ src/python/clik_join.py
@@ -0,0 +1,37 @@
+#! $PYTHONEXE
+import sys
+sys.path = ["$REPLACEPATH"]+sys.path
+
+import numpy as nm
+import clik
+import h5py 
+
+def main(argv):
+  if len(sys.argv)<4:
+    print "usage : %s lkl_file_1 lkl_file_2 [lkl_file_3 ...] result_lkl_file"
+    sys.exit(1)
+  lkls = [h5py.File(ll)["clik"] for ll in sys.argv[1:-1]]
+  reslkl = h5py.File(sys.argv[-1],"w")
+  
+  nlkl = 0
+  lmax = -nm.ones(6)
+  resclik = reslkl.create_group("clik")
+
+  for lklin in lkls:
+    lmaxin = lklin.attrs["lmax"]
+    lmax = nm.max((lmax,lmaxin),0)
+    nlklin = lklin.attrs["n_lkl_object"]
+    
+    for i in range(nlklin):
+      grpin = "lkl_%d"%i
+      grpout = "lkl_%d"%nlkl
+      nlkl+=1
+      lklin.copy(lklin[grpin],resclik,grpout)
+  
+  resclik.attrs["lmax"] = lmax
+  resclik.attrs["n_lkl_object"] = nlkl
+  
+  reslkl.close()
+    
+if __name__=="__main__":
+  main(sys.argv)
