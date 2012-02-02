dce76d20a119ccf41a55751512b613e937dadd22
Karim Benabed <benabed@iap.fr>
2012-02-02 16:12:37 +0000

    debug icc

--- waf_tools/try_icc.py
+++ waf_tools/try_icc.py
@@ -1,3 +1,9 @@
+import Options
+from waflib import Logs
+from waflib import  Context
+from waflib import Errors
+import re  
+  
 def options(ctx):
   import optparse
   grp = ctx.parser.get_option_group("--gfortran")
@@ -7,26 +13,50 @@ def options(ctx):
   grp.add_option("--icc",action="store_true",default=False,help="Do not test for gcc and only use icc")
   ctx.add_option_group(grp)  
 
+def do_icc(ctx):
+  ctx.env.CC=[]
+  ctx.env.LINK_CC=[]
+  ctx.check_tool('icc')
+  ctx.check_cc(
+      errmsg="failed",msg="Compile a test code with icc",
+      mandatory=1,fragment = "#include <stdio.h>\nmain() {fprintf(stderr,\"hello world\");}\n",compile_filename='test.c',features='c cprogram')
+  ctx.env.has_icc = True
+
+def do_gcc(ctx):
+  ctx.env.CC=[]
+  ctx.env.LINK_CC=[]
+  ctx.check_tool('gcc')
+  ctx.start_msg("Check gcc version") 
+  v90 = ctx.cmd_and_log(ctx.env.CC[0]+" --version",quiet=Context.STDOUT).split("\n")[0].strip()
+  version90 = re.findall("(4\.[0-9]\.[0-9])",v90)
+  if len(version90)<1:
+    #Logs.pprint("PINK","Can't get gfortran version... Let's hope for the best")
+    ctx.end_msg("not found, let's hope for the best...",color="PINK")
+  else:
+    version90 = version90[0]
+    vmid = int(version90.split(".")[1])
+    if vmid<2:
+      ctx.end_msg(v90,color="YELLOW")
+      raise Errors.WafError("gcc version need to be above 4.2 got %s"%version90)
+    ctx.end_msg(v90)
+  ctx.check_cc(
+    errmsg="failed",msg="Compile a test code with gcc",
+    mandatory=1,fragment = "#include <stdio.h>\nmain() {fprintf(stderr,\"hello world\");}\n",compile_filename='test.c',features='c cprogram')
+
+
 def configure_iccfirst(ctx):
   import Options
   from waflib import Logs
   ctx.env.has_icc = False
   if not Options.options.gcc:
     try:
-      ctx.check_tool('icc')
-      ctx.check_cc(
-        errmsg="failed",msg="Compile a test code with icc",
-        mandatory=1,fragment = "#include <stdio.h>\nmain() {fprintf(stderr,\"hello world\");}\n",compile_filename='test.c',features='c cprogram')
-      ctx.env.has_icc = True
+      do_icc(ctx)    
       return
     except:
       if Options.options.icc:
         raise
       Logs.pprint("PINK", "icc not found, defaulting to gcc")
-  ctx.check_tool('gcc')
-  ctx.check_cc(
-        errmsg="failed",msg='Compile a test code with gcc',
-        mandatory=1,fragment = "#include <stdio.h>\nmain() {fprintf(stderr,\"hello world\");}\n",compile_filename='test.c',features='c cprogram')
+  do_gcc(ctx)
 
 
 def configure_gccfirst(ctx):
@@ -39,32 +69,13 @@ def configure_gccfirst(ctx):
   import re  
   if not Options.options.icc:
     try:
-      ctx.check_tool('gcc')
-      ctx.start_msg("Check gcc version") 
-      v90 = ctx.cmd_and_log(ctx.env.CC[0]+" --version",quiet=Context.STDOUT).split("\n")[0].strip()
-      version90 = re.findall("(4\.[0-9]\.[0-9])",v90)
-      if len(version90)<1:
-        #Logs.pprint("PINK","Can't get gfortran version... Let's hope for the best")
-        ctx.end_msg("not found, let's hope for the best...",color="PINK")
-      else:
-        version90 = version90[0]
-        vmid = int(version90.split(".")[1])
-        if vmid<2:
-          ctx.end_msg(v90,color="YELLOW")
-          raise Errors.WafError("gcc version need to be above 4.2 got %s"%version90)
-        ctx.end_msg(v90)
-      ctx.check_cc(
-        errmsg="failed",msg="Compile a test code with gcc",
-        mandatory=1,fragment = "#include <stdio.h>\nmain() {fprintf(stderr,\"hello world\");}\n",compile_filename='test.c',features='c cprogram')
+      do_gcc(ctx)  
       return
     except Exception,e:
       if Options.options.gcc:
         raise
       Logs.pprint("PINK", "gcc not found, defaulting to icc (cause : %s)"%e)
-  ctx.check_tool('icc')
-  ctx.check_cc(
-        errmsg="failed",msg='Compile a test code with icc',
-        mandatory=1,fragment = "#include <stdio.h>\nmain() {fprintf(stderr,\"hello world\");}\n",compile_filename='test.c',features='c cprogram')
-  ctx.env.has_icc = True
 
+  do_icc(ctx)
+  
 configure = configure_gccfirst
