c421cb8dfc74f3177e2828f669241d2827fc60cd
lejeune <devnull@localhost>
2012-02-21 13:08:59 +0000

    include mvdens and use data dir

--- src/egfs/standalone/setup.py
+++ src/egfs/standalone/setup.py
@@ -12,17 +12,26 @@ import os
 # gfortran -print-file-name=libgfortran.dylib )
 
 fruntime = "/usr/local/gfortran/lib/gcc/i386-apple-darwin8.10.1/4.5.0/../../../libgfortran.dylib"
-
+fruntime = "/usr/lib/gcc/x86_64-linux-gnu/4.4.5/libgfortran.so"
 
 Lfruntime = osp.split(fruntime)[0]
 Lfruntime = osp.realpath(Lfruntime)
 lfruntime = "gfortran"
 
 setup(
+    name='egfs',
+    version='1.0',
+    packages=['egfs'],
+    package_dir={'egfs': '.'},
+    package_data={'egfs': ['data/*.dat']},
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("egfs", ["egfs.pyx","clik_egfs.c","distribution.c","io.c","errorlist.c"],
+    ext_modules = [Extension("egfs", ["egfs.pyx","clik_egfs.c","distribution.c","io.c","errorlist.c", "mvdens.c"],
                              include_dirs = [nm.get_include()],
                              library_dirs=['.',Lfruntime],
-                             libraries=["egfs",lfruntime]
+                             libraries=["egfs",lfruntime, "gsl", "gslcblas" ]
+                             ),
+    			   Extension("parametric", ["parametric.pyx","clik_parametric.c","io.c","errorlist.c"],
+                             include_dirs = [nm.get_include()],
+                             library_dirs=['.',Lfruntime]
                              )],
 )
