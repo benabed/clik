3a21c393a4febe69ce5d4b38f18ef6df2eb83951
Simon Prunet <prunet@iap.fr>
2012-05-21 09:20:28 +0000

    Debug. Should be used with the 19 usual detectors only !!

--- src/component_plugin/psm_diffuse_bydet/psm_diffuse_bydet.pyx
+++ src/component_plugin/psm_diffuse_bydet/psm_diffuse_bydet.pyx
@@ -1,6 +1,6 @@
 from clik.parametric cimport c_parametric, error, doError, parametric, parametric_template
 
-cdef extern c_parametric *psm_diffuse_bydet_init(int ndet, char **detnames, int ndef, char** defkey, char **defvalue, int nvar, char **varkey, int lmin, int lmax, double* rq_in, error **err)
+cdef extern c_parametric *psm_diffuse_bydet_init(int ndet, double *detlist, int ndef, char** defkey, char **defvalue, int nvar, char **varkey, int lmin, int lmax, double* rq_in, error **err)
 
 cdef class psm_diffuse_bydet(parametric_template):
   def __cinit__(self):
