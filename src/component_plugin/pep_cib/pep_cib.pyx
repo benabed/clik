d62a9d1cab040d796ec1ae2dc2186031839db65c
Simon Prunet <prunet@iap.fr>
2012-07-17 10:42:59 +0000

    Added ir_clustered_pep2 component, with 143x217 cross generated as 0.95 * harmonic_mean(143,217), and 143x353 cross as 0.95 * cross_corr(217,353) * harmonic_mean(143,353)
    where cross_corr(i,j) = C_ij/sqrt(C_ii*C_jj)
    harmonic_mean(i,j) = sqrt(C_ii*C_jj)

--- src/component_plugin/pep_cib/pep_cib.pyx
+++ src/component_plugin/pep_cib/pep_cib.pyx
@@ -1,6 +1,7 @@
 from clik.parametric cimport c_parametric, error, doError, parametric, parametric_template
 
 cdef extern c_parametric *ir_clustered_pep_init(int ndet, double *detlist, int ndef, char** defkey, char **defvalue, int nvar, char **varkey, int lmin, int lmax, double* rq_clustered_in, error **err)
+cdef extern c_parametric *ir_clustered_pep2_init(int ndet, double *detlist, int ndef, char** defkey, char **defvalue, int nvar, char **varkey, int lmin, int lmax, double* rq_clustered_in, error **err)
 cdef extern c_parametric *ir_poisson_pep_init(int ndet, double *detlist, int ndef, char** defkey, char **defvalue, int nvar, char **varkey, int lmin, int lmax, double* rq_clustered_in, error **err)
 
 cdef class ir_clustered_pep(parametric_template):
@@ -9,10 +10,16 @@ cdef class ir_clustered_pep(parametric_template):
     self.template_name = "ir_clustered_pep.dat"
     self.plugin_name = "pep_cib"
 
+cdef class ir_clustered_pep2(parametric_template):
+  def __cinit__(self):
+    self.initfunc = <void*> ir_clustered_pep2_init;
+    self.template_name = "ir_clustered_pep2.dat"
+    self.plugin_name = "pep_cib"
+
 cdef class ir_poisson_pep(parametric_template):
   def __cinit__(self):
     self.initfunc = <void*> ir_poisson_pep_init;
     self.template_name = "ir_poisson_pep.dat"
     self.plugin_name = "pep_cib"
 
-component_list = ["ir_poisson_pep","ir_clustered_pep"]
+component_list = ["ir_poisson_pep","ir_clustered_pep","ir_clustered_pep2"]
