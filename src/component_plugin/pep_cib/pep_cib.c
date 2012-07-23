ca7564a1b791f773bc81677565baa83da2b94df8
Simon Prunet <prunet@iap.fr>
2012-07-23 08:07:35 +0000

    Added Guilaine Poisson IR model (not PEP)

--- src/component_plugin/pep_cib/pep_cib.c
+++ src/component_plugin/pep_cib/pep_cib.c
@@ -177,6 +177,59 @@ parametric *ir_poisson_pep_init(int ndet, double *detlist, int ndef, char** defk
   return egl;
 }
 
+void ir_poisson_guilaine_compute(parametric* egl, double *Rq, error **err) {
+  int ell,m1,m2,mell,nfreq,iv,mv,lell;
+  double ir_poisson_pep_norm;
+  double *A;
+  int *ind_freq;
+  int ind1, ind2;
+  template_payload *payload;
+  int nfreqs_hfi = 6;
+
+  payload = egl->payload;
+  A = payload->template; // Stores input C_l(nu,nu') for all HFI frequencies
+  nfreq = egl->nfreq;
+  ind_freq = payload->ind_freq;
+
+  ir_poisson_pep_norm = parametric_get_value(egl,"ir_poisson_guilaine_norm",err);
+  forwardError(*err,__LINE__,);
+
+  for (ell=egl->lmin;ell<=egl->lmax;ell++) {
+    for (m1=0;m1<nfreq;m1++) {
+      for (m2=m1;m2<nfreq;m2++) {
+        ind1 = ind_freq[m1];
+        ind2 = ind_freq[m2];
+        Rq[IDX_R(egl,ell,m1,m2)] = ir_poisson_pep_norm * A[ind1*nfreqs_hfi + ind2];
+        Rq[IDX_R(egl,ell,m2,m1)] = Rq[IDX_R(egl,ell,m1,m2)];
+      }
+    }
+  }
+  return;
+}
+
+parametric *ir_poisson_guilaine_init(int ndet, double *detlist, int ndef, char** defkey, char **defvalue, int nvar, char **varkey, int lmin, int lmax, double* template, error **err) {
+  parametric *egl;
+  double hfi_freqlist[6] = {100.,143.,217.,353.,545.,857.};
+  int nfreqs_hfi = 6;
+  
+  egl = parametric_init(ndet, detlist, ndef, defkey, defvalue, nvar, varkey, lmin, lmax, err);
+  forwardError(*err,__LINE__,NULL);
+  
+  parametric_template_payload_init(egl,template, (nfreqs_hfi*nfreqs_hfi),hfi_freqlist,nfreqs_hfi,err);
+  forwardError(*err,__LINE__,NULL);
+  
+  egl->eg_compute = &ir_poisson_pep_compute;
+  egl->eg_free = &parametric_template_payload_free;
+
+  parametric_set_default(egl,"ir_poisson_guilaine_norm",1.0,err);
+  forwardError(*err,__LINE__,NULL);
+  parametric_add_derivative_function(egl,"ir_poisson_guilaine_norm",&parametric_norm_derivative,err);
+  forwardError(*err,__LINE__,NULL);
+
+  return egl;
+}
+
 CREATE_PARAMETRIC_TEMPLATE_FILE_INIT(ir_clustered_pep,ir_clustered_pep_init);
 CREATE_PARAMETRIC_TEMPLATE_FILE_INIT(ir_clustered_pep2,ir_clustered_pep2_init);
 CREATE_PARAMETRIC_TEMPLATE_FILE_INIT(ir_poisson_pep,ir_poisson_pep_init);
+CREATE_PARAMETRIC_TEMPLATE_FILE_INIT(ir_poisson_guilaine,ir_poisson_guilaine_init);
--- src/component_plugin/pep_cib/pep_cib.pyx
+++ src/component_plugin/pep_cib/pep_cib.pyx
@@ -3,6 +3,7 @@ from clik.parametric cimport c_parametric, error, doError, parametric, parametri
 cdef extern c_parametric *ir_clustered_pep_init(int ndet, double *detlist, int ndef, char** defkey, char **defvalue, int nvar, char **varkey, int lmin, int lmax, double* rq_clustered_in, error **err)
 cdef extern c_parametric *ir_clustered_pep2_init(int ndet, double *detlist, int ndef, char** defkey, char **defvalue, int nvar, char **varkey, int lmin, int lmax, double* rq_clustered_in, error **err)
 cdef extern c_parametric *ir_poisson_pep_init(int ndet, double *detlist, int ndef, char** defkey, char **defvalue, int nvar, char **varkey, int lmin, int lmax, double* rq_clustered_in, error **err)
+cdef extern c_parametric *ir_poisson_guilaine_init(int ndet, double *detlist, int ndef, char** defkey, char **defvalue, int nvar, char **varkey, int lmin, int lmax, double* rq_clustered_in, error **err)
 
 cdef class ir_clustered_pep(parametric_template):
   def __cinit__(self):
@@ -22,4 +23,10 @@ cdef class ir_poisson_pep(parametric_template):
     self.template_name = "ir_poisson_pep.dat"
     self.plugin_name = "pep_cib"
 
-component_list = ["ir_poisson_pep","ir_clustered_pep","ir_clustered_pep2"]
+cdef class ir_poisson_guilaine(parametric_template):
+  def __cinit__(self):
+    self.initfunc = <void*> ir_poisson_guilaine_init;
+    self.template_name = "ir_poisson_guilaine.dat"
+    self.plugin_name = "pep_cib"
+
+component_list = ["ir_poisson_pep","ir_poisson_guilaine","ir_clustered_pep","ir_clustered_pep2"]
