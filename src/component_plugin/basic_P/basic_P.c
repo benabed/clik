bdc0e04dc47b40424c12fb84c96966aa5ea2f7d4
Karim Benabed <benabed@iap.fr>
2013-07-05 16:12:08 +0000

    add pw_EE

--- src/component_plugin/basic_P/basic_P.c
+++ src/component_plugin/basic_P/basic_P.c
@@ -8,8 +8,6 @@ typedef struct {
   char **nrm_names;
 } pw_XX_payload;
 
-void pw_TE_compute(parametric* exg, double *Rq, error **err);
-
 void pw_XX_free(void **PP) {
   pw_XX_payload *P;
 
@@ -22,6 +20,7 @@ void pw_XX_free(void **PP) {
   free(P);
   *PP = NULL;
 }
+void pw_TE_compute(parametric* exg, double *Rq, error **err);
 
 parametric *pw_TE_init(int ndet_T, int ndet_P, int *has_TEB, double *detlist, int ndef, char** defkey, char **defvalue, int nvar, char **varkey, int lmin, int lmax, error **err) {
   parametric *egl;
@@ -172,4 +171,153 @@ void pw_TE_compute(parametric* egl, double *Rq, error **err) {
   }
 }
 
+void pw_EE_compute(parametric* exg, double *Rq, error **err);
+
+parametric *pw_EE_init(int ndet_T, int ndet_P, int *has_TEB, double *detlist, int ndef, char** defkey, char **defvalue, int nvar, char **varkey, int lmin, int lmax, error **err) {
+  parametric *egl;
+  int mx,i,j;
+  pw_XX_payload *payload;
+  int hk;
+  char **chn, *bf,*cbf,*pch;
+  char *channeln;
+  int tl, mm, lbf,l,delta;
+
+  egl = parametric_pol_init(ndet_T, ndet_P, has_TEB, detlist, ndef, defkey, defvalue, nvar, varkey, lmin, lmax, err);
+  forwardError(*err,__LINE__,NULL);
+  
+  egl->eg_compute = &pw_EE_compute;
+  egl->eg_free = &pw_XX_free;
+
+  egl->payload  = malloc_err(sizeof(pw_XX_payload),err);
+  forwardError(*err,__LINE__,NULL);
+    
+  payload = egl->payload;
+  payload->n = (egl->nfreq_P*(egl->nfreq_P+1))/2;
+  mx = egl->nfreq_T + egl->nfreq_P;
+
+  payload->nrm = malloc_err(sizeof(double)*mx,err);
+  forwardError(*err,__LINE__,NULL);
+  payload->m1 = malloc_err(sizeof(int)*payload->n,err);
+  forwardError(*err,__LINE__,NULL);
+  payload->m2 = malloc_err(sizeof(int)*payload->n,err);
+  forwardError(*err,__LINE__,NULL);
+  
+  mx = 0;
+  for(i=0;i<egl->nfreq_P;i++) {
+    for(j=i;j<egl->nfreq_P;j++) {
+      payload->m1[mx] = i + egl->nfreq_T;
+      payload->m2[mx] = j + egl->nfreq_T;
+      mx++;
+    }
+  }
+
+  mx = egl->nfreq_P;
+
+  parametric_set_default(egl,"pw_EE_index",-1,err);
+  forwardError(*err,__LINE__,NULL);
+  
+  parametric_set_default(egl,"pw_EE_l_pivot",500,err);
+  forwardError(*err,__LINE__,NULL);
+  
+  bf = malloc_err(sizeof(char)*mx*256,err);
+  forwardError(*err,__LINE__,NULL);
+  cbf = malloc_err(sizeof(char)*mx*256,err);
+  forwardError(*err,__LINE__,NULL);
+  chn = malloc_err(sizeof(char*)*mx,err);
+  forwardError(*err,__LINE__,NULL);
+
+  for(i=0;i<egl->nfreq_P;i++) {
+    sprintf(cbf,"%s,%d_E",cbf,i);
+  }
+
+  hk = cdic_key_index(egl->pf,"pw_EE_channel_names",err);
+  forwardError(*err,__LINE__,NULL);
+  
+  channeln = cbf;
+
+  
+  if (hk!=-1) {
+    channeln = cdic_get(egl->pf,"pw_EE_channel_names",NULL,err);
+    forwardError(*err,__LINE__,NULL);  
+  }
+  
+  tl = strlen(channeln);
+  mm = 0;
+  lbf = 0;
+  l=0;
+  while(l<tl) {
+    chn[mm] = bf + lbf;
+    while(channeln[l]==' ' || channeln[l]==',') {
+      l++;
+    }
+    while(l<tl && channeln[l]!=',') {
+      bf[lbf] = channeln[l];
+      l++;
+      lbf++;
+    }
+    bf[lbf] = '\0';
+    lbf++;
+    //_DEBUGHERE_("%d |%s|",mm,chn[mm]);
+    mm++;
+  }
+  testErrorRet(mm!=mx,-97664,"bad pw_EE_channel_names value",*err,__LINE__,NULL);
+
+  pch = malloc_err(sizeof(char)*mx*256,err);
+  forwardError(*err,__LINE__,NULL);  
+
+  payload->nrm_names = malloc_err(sizeof(char*)*mx,err);
+  forwardError(*err,__LINE__,NULL);  
+
+  delta = 0;
+
+  for(i=0;i<mx;i++) {
+    payload->nrm_names[i] = pch + delta;
+    sprintf(payload->nrm_names[i],"pw_EE_A_%s",chn[i]);
+    parametric_set_default(egl,payload->nrm_names[i],1.,err);
+    forwardError(*err,__LINE__,NULL); 
+   
+    delta = strlen(payload->nrm_names[i]) +1;
+    pch = payload->nrm_names[i];
+  }      
+
+  free(bf);
+  free(cbf);
+  return egl;
+}
+
+void pw_EE_compute(parametric* egl, double *Rq, error **err) {
+  pw_XX_payload *payload;
+  int i,mx;
+  int ell,m1,m2;
+  double *A;
+  double l_pivot, index,v;
+
+  payload = egl->payload;
+  mx = egl->nfreq_P;
+  
+
+  for(i=0;i<mx;i++) {
+    payload->nrm[i] = parametric_get_value(egl,payload->nrm_names[i],err);
+    forwardError(*err,__LINE__,);
+  }
+  A = payload->nrm;
+
+  l_pivot = parametric_get_value(egl,"pw_EE_l_pivot",err);
+  forwardError(*err,__LINE__,);
+
+  index = parametric_get_value(egl,"pw_EE_index",err);
+  forwardError(*err,__LINE__,);
+
+  for (ell=egl->lmin;ell<=egl->lmax;ell++) {
+    v = pow((double) ell/l_pivot,(double) index);
+    for(i=0;i<payload->n;i++) {
+      m1 = payload->m1[i];
+      m2 = payload->m2[i];
+      Rq[IDX_R(egl,ell,m1,m2)] = A[m1 - egl->nfreq_T] * A[m2- egl->nfreq_T] * v;
+      Rq[IDX_R(egl,ell,m2,m1)] = Rq[IDX_R(egl,ell,m1,m2)];
+    }
+  }
+}
+
 CREATE_PARAMETRIC_POL_FILE_INIT(pw_TE,pw_TE_init);
+CREATE_PARAMETRIC_POL_FILE_INIT(pw_EE,pw_EE_init);
--- src/component_plugin/basic_P/basic_P.pyx
+++ src/component_plugin/basic_P/basic_P.pyx
@@ -1,9 +1,14 @@
 from clik.parametric cimport c_parametric, error, doError, parametric, parametric_template, parametric_pol, parametric_pol_template
 
 cdef extern c_parametric *pw_TE_init(int ndet_T, int ndet_P, int *has_TEB, double *detlist, int ndef, char** defkey, char **defvalue, int nvar, char **varkey, int lmin, int lmax, error **err)
+cdef extern c_parametric *pw_EE_init(int ndet_T, int ndet_P, int *has_TEB, double *detlist, int ndef, char** defkey, char **defvalue, int nvar, char **varkey, int lmin, int lmax, error **err)
 
 cdef class pw_TE(parametric_pol):
   def __cinit__(self):
     self.initfunc = <void*>pw_TE_init
 
-component_list = ["pw_TE"]
+cdef class pw_EE(parametric_pol):
+  def __cinit__(self):
+    self.initfunc = <void*>pw_EE_init
+
+component_list = ["pw_TE","pw_EE"]
