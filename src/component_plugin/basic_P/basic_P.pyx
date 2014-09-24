e63ebb4f91c33a94c3fcdf847817c26d33159e64
Karim Benabed <benabed@gmail.com>
2014-09-24 18:48:05 +0200

    dalf

--- src/component_plugin/basic_P/basic_P.pyx
+++ src/component_plugin/basic_P/basic_P.pyx
@@ -28,8 +28,14 @@ def galf_P_rename_func(v,rups):
     rv = v.replace("galf","pwfe")
     rups[v]=rv
 
-galf_TE = rename_machine(powerlaw_free_emissivity_TE,{"galf_TE_l2_norm":"1","galf_TE_l_pivot":"500","ps_TE_index":"-2.4"},galf_P_rename_func)
-galf_EE = rename_machine(powerlaw_free_emissivity_EE,{"galf_EE_l2_norm":"1","galf_EE_l_pivot":"500","ps_EE_index":"-2.4"},galf_P_rename_func)
+def dalf_P_rename_func(v,rups):
+  if v.startswith("dalf"):
+    rv = v.replace("dalf","pwfe")
+    rups[v]=rv
+
+galf_TE = rename_machine(powerlaw_free_emissivity_TE,{"galf_TE_l2_norm":"1","galf_TE_l_pivot":"500","galf_TE_index":"-2.4"},galf_P_rename_func)
+galf_EE = rename_machine(powerlaw_free_emissivity_EE,{"galf_EE_l2_norm":"1","galf_EE_l_pivot":"500","galf_EE_index":"-2.4"},galf_P_rename_func)
+dalf_EE = rename_machine(powerlaw_free_emissivity_EE,{"dalf_EE_l2_norm":"1","dalf_EE_l_pivot":"500","dalf_EE_index":"-2."},dalf_P_rename_func)
 
 def ps_P_rename_func(v,rups):
   if v.startswith("ps"):
@@ -39,4 +45,4 @@ def ps_P_rename_func(v,rups):
 ps_TE = rename_machine(powerlaw_free_emissivity_TE,{"ps_TE_l2_norm":"1","ps_TE_l_pivot":"3000","ps_TE_index":"0"},ps_P_rename_func)
 ps_EE = rename_machine(powerlaw_free_emissivity_EE,{"ps_EE_l2_norm":"1","ps_EE_l_pivot":"3000","ps_EE_index":"0"},ps_P_rename_func)
 
-component_list = ["pw_TE","pw_EE","powerlaw_free_emissivity_EE","powerlaw_free_emissivity_TE","galf_TE","galf_EE","ps_TE","ps_EE"]
+component_list = ["pw_TE","pw_EE","powerlaw_free_emissivity_EE","powerlaw_free_emissivity_TE","galf_TE","galf_EE","ps_TE","ps_EE","dalf_EE"]
