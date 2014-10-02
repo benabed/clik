a3a749f11d0d45ebc45619303e8674101b9c406b
Karim Benabed <benabed@gmail.com>
2014-10-02 22:09:47 +0200

    add more lapack functions for bflike

--- waf_tools/any_lapack.py
+++ waf_tools/any_lapack.py
@@ -10,7 +10,7 @@ import waflib
 
 version = "lapack-3.3.1"
 tool = "lapack-3.3.1"
-lapack_funcs = "dposv dtrsv dpotrf dpotrs dpotri dtrtri dtrmm dtrmv dgeqrf dormqr dsyev dgesvd dsymv dgemv dgemm dsyrk dsyr2k daxpy dtrsm dsymm dsyr ddot dsyevd"     
+lapack_funcs = "dposv dtrsv dpotrf dpotrs dpotri dtrtri dtrmm dtrmv dgeqrf dormqr dsyev dgesvd dsymv dgemv dgemm dsyrk dsyr2k daxpy dtrsm dsymm dsyr ddot dsyevd dlamch dsyevr"     
 
 def options(ctx):
   atl.add_lib_option("lapack",ctx,install=True)
