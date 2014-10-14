d0c949018f6458887036c4c8838141c6466259b2
Karim Benabed <benabed@gmail.com>
2014-10-14 23:14:16 +0200

    we're in 2014

--- src/component_plugin/cibsz/cibsz.pyx
+++ src/component_plugin/cibsz/cibsz.pyx
@@ -90,12 +90,12 @@ cdef class cibr(parametric):
     
 
 cib_1h_2h = rename_machine(gcib,{},rename_replace("gib","cib"),data_file="cib_1h_2h_100_353_Jsr-1_GL_2014_2.dat")
-cib_1h_2h_sept04 = rename_machine(gcib,{},rename_replace("gib","cib"),data_file="cib_1h_2h_100_353_Jsr-1_PS_2014_09.dat")
+cib_1h_2h_sept14 = rename_machine(gcib,{},rename_replace("gib","cib"),data_file="cib_1h_2h_100_353_Jsr-1_PS_2014_09.dat")
 cib_1h_2h_SN = rename_machine(gcib,{},rename_replace("gib","cib"),data_file="cib_tot_100_353_Jsr-1_GL_2013_10.dat")
 cibsz_1h_2h = rename_machine(gcib,{"cibsz_rigid":"143"},rename_replace("gib","cibsz"),data_file="cibsz_1h_2h_100_353_Jsr-1_GL_2014_2.dat")
-cibsz_1h_2h_sept04 = rename_machine(gcib,{"cibsz_rigid":"143"},rename_replace("gib","cibsz"),data_file="cibsz_1h_2h_100_353_Jsr-1_PS_2014_09.dat")
+cibsz_1h_2h_sept14 = rename_machine(gcib,{"cibsz_rigid":"143"},rename_replace("gib","cibsz"),data_file="cibsz_1h_2h_100_353_Jsr-1_PS_2014_09.dat")
 
 
 
       
-component_list = ["cibsz_1h_2h","cib_1h_2h","cib_1h_2h_SN","gcib","ncib","cib","cibr","sz","sz_cib","sz_x","cib_x","sz_cib_x","ksz","ncibXsz","tcib","cib_1h_2h_sept04","cibsz_1h_2h_sept04"]
+component_list = ["cibsz_1h_2h","cib_1h_2h","cib_1h_2h_SN","gcib","ncib","cib","cibr","sz","sz_cib","sz_x","cib_x","sz_cib_x","ksz","ncibXsz","tcib","cib_1h_2h_sept14","cibsz_1h_2h_sept14"]
