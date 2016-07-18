35979b4df91d9aca93cd8831fd60890e7630c346
Karim Benabed <benabed@gmail.com>
2016-07-18 16:05:03 +0200

    dbg A_cmb

--- src/python/clik/smicahlp.py
+++ src/python/clik/smicahlp.py
@@ -457,12 +457,13 @@ def add_parametric_component(lkl_grp,name,dets,vpars,lmin,lmax,defaults={},color
   nT = lkl_grp.attrs["m_channel_T"]
   nP = lkl_grp.attrs["m_channel_P"]
   A_cmb = lkl_grp.attrs["A_cmb"]
+  has_cl = lkl_grp.attrs["has_cl"]
+  if has_cl[1]*has_cl[2]:
+    A_cmb = nm.concatenate((A_cmb[:nT],A_cmb[nT:],A_cmb[nT:]))
+  
   if issubclass(prclass,parametric.parametric_pol):
-    has_cl = lkl_grp.attrs["has_cl"]
     pm = prclass(dets[:nT],dets[nT:],has_cl,vpars,lmin,lmax,defaults,color=color,rename=rename,voidmask=voidmask)
-    if has_cl[1]*has_cl[2]:
-      A_cmb = nm.concatenate((A_cmb[:nT],A_cmb[nT:],A_cmb[nT:]))
-
+    
   else:
     dets = dets[:nT]
     if color!=None:
