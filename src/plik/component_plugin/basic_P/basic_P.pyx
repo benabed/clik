9278f79ed083b6a7c4006cbb629f5dce4c7453d0
Karim Benabed <benabed@gmail.com>
2016-09-26 17:25:41 +0200

    add new component for dust cleaned maps

--- src/plik/component_plugin/basic_P/basic_P.pyx
+++ src/plik/component_plugin/basic_P/basic_P.pyx
@@ -33,6 +33,16 @@ def galf_P_rename_func(v,rups):
     rv = v.replace("galf","pwfe")
     rups[v]=rv
 
+def galres_TT_rename_func(v,rups):
+  if v.startswith("galres_TT"):
+    rv = v.replace("galres_TT","pwfe")
+    rups[v]=rv
+
+def galres_TE_rename_func(v,rups):
+  if v.startswith("galres_TE"):
+    rv = v.replace("galres_TE","pwfe_XX")
+    rups[v]=rv
+
 def dalf_P_rename_func(v,rups):
   if v.startswith("dalf"):
     rv = v.replace("dalf","pwfe")
@@ -56,6 +66,8 @@ galx_TE = rename_machine(powerlaw_free_emissivity_XX,{"galf_TE_l2_norm":"1","gal
 galx_TB = rename_machine(powerlaw_free_emissivity_XX,{"galf_TB_l2_norm":"1","galf_TB_l_pivot":"500","galf_TB_index":"-2.4","galf_TB_kind":"4"},mk_galx_P_rename_func("TB"))
 galx_EB = rename_machine(powerlaw_free_emissivity_XX,{"galf_EB_l2_norm":"1","galf_EB_l_pivot":"500","galf_EB_index":"-2.4","galf_EB_kind":"5"},mk_galx_P_rename_func("EB"))
 
+galres_TT = rename_machine(powerlaw_free_emissivity,{"galres_TT_l2_norm":"1","galres_TT_l_pivot":"150","galres_TT_index":"-2.5"},galres_TT_rename_func)
+galres_TE = rename_machine(powerlaw_free_emissivity_XX,{"galres_TE_l2_norm":"1","galres_TE_l_pivot":"150","galres_TE_index":"-3.1","galres_TE_kind":"3"},galres_TE_rename_func)
 
 def ps_P_rename_func(v,rups):
   if v.startswith("ps"):
@@ -65,4 +77,4 @@ def ps_P_rename_func(v,rups):
 ps_TE = rename_machine(powerlaw_free_emissivity_TE,{"ps_TE_l2_norm":"1","ps_TE_l_pivot":"3000","ps_TE_index":"0"},ps_P_rename_func)
 ps_EE = rename_machine(powerlaw_free_emissivity_EE,{"ps_EE_l2_norm":"1","ps_EE_l_pivot":"3000","ps_EE_index":"0"},ps_P_rename_func)
 
-component_list = ["powerlaw_free_emissivity_XX","pw_TE","pw_EE","powerlaw_free_emissivity_EE","powerlaw_free_emissivity_TE","galf_TE","galf_EE","ps_TE","ps_EE","dalf_EE","galx_EE","galx_BB","galx_TE","galx_TB","galx_EB"]
+component_list = ["galres_TT","galres_TE","powerlaw_free_emissivity_XX","pw_TE","pw_EE","powerlaw_free_emissivity_EE","powerlaw_free_emissivity_TE","galf_TE","galf_EE","ps_TE","ps_EE","dalf_EE","galx_EE","galx_BB","galx_TE","galx_TB","galx_EB"]
